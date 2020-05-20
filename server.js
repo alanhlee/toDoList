4744a2ed3e5876abb126a2173742565f1deb6bac
Erika <erika.paige.handley@gmail.com>
2020-05-20 12:39:56 -0700

    push newItem into array

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-let items = ['shop', 'clean']
+let items = []
 
 //get route to get all items, return an array 
 app.get('/api/items', (req, res) => {
@@ -12,22 +12,23 @@ app.get('/api/items', (req, res) => {
 
 app.put('/api/createItem', (req, res) =>{
   
+  // req.body = info from ajax
   let newItem = req.body
-  let id =1
+  let id = 1
 
   let idItems = false
 
   //create a way to check the ID has not been used (create unique ID)
   for (let i =0; i < items.length; i++) {
     if items[i].id === id {
-      console.log(true)
+      console.log('do nothing')
     } else {
-      items[i].id !== id
-      items.push[]
+      id++
+      items.push(newItem)
     }
   }
-  
   res.send(200, items)
 })
+
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
