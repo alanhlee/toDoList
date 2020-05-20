ca97cf4ca7036fbbc3fdde6827b0c81e018675ae
Erika <erika.paige.handley@gmail.com>
2020-05-20 12:02:23 -0700

    for loop

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-
-
 let items = ['shop', 'clean']
 
 //get route to get all items, return an array 
@@ -15,8 +13,18 @@ app.get('/api/items', (req, res) => {
 app.put('/api/createItem', (req, res) =>{
   
   let newItem = req.body
+  let id =1
+
+  let idItems = false
 
   //create a way to check the ID has not been used (create unique ID)
+  for (let i =0; i < items.length; i++) {
+    if items[i].id === id {
+      console.log(true)
+    } else {
+      items.push[]
+    }
+  }
   
   res.send(200, items)
 })
