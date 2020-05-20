0ea243261aa86ee5105e3593713750066e3f8f15
Maxwell Roberts <maxwell.k.roberts@gmail.com>
2020-05-20 13:58:03 -0400

    wrote put and delete routes

--- server.js
+++ server.js
@@ -2,7 +2,9 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path')
 
+app.use(express.static(path.join(__dirname,'/public')))
 app.use(express.urlencoded())
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
@@ -14,11 +16,16 @@ let testItem = {
   uniqueID: 666
 }
 
-app.put('modifyItem',(req,res) => { //modify an existing item
-  myNewItem = req.body //this req should pass in an item object with text, isDone, and uniqueID properties
+app.put('/modifyItem',(req,res) => { //modify an existing item
+  let myNewItem = req.body //this req should pass in an item object with text, isDone, and uniqueID properties
   let indexToReplace = arrayOfItems.findIndex(item => item.uniqueID == myNewItem.uniqueID)
-  arrayOfItems.splice(myNewIte
+  arrayOfItems.splice(indexToReplace, 1, myNewItem)
+  res.send.(200,arrayOfItems)
 })
-app.delete('/api/items',(req,res) => { //delete an item
 
+app.delete('/api/items',(req,res) => { //delete an item
+  let myID = req.body //this req should pass in a unique ID
+  let indexToReplace = arrayOfItems.findIndex(item => item.uniqueID == myNewItem.uniqueID)
+  arrayOfItems.splice(indexToReplace, 1)
+  res.send.(200, arrayOfItems)
 })
